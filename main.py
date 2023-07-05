7a7acd86ff7ed23f76aa7ac139a2c709b4a5c813
IñigoBerasa <124163362+Iberasa010@users.noreply.github.com>
2023-07-05 12:12:17 +0200

    Initial commit

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 
-julie_list = []
-james_list = []
-sarah_list = []
-mickey_list = []
-
 try:
-    with open('julie.txt') as julie:
-        print(julie.readline())
     with open('james.txt') as james:
-        print(james.readline())
-    with open('mickey.txt') as mickey:
-        print(mickey.readline())
+        data = james.readline()
+        james_data = data.strip().split(',')
+        print(james_data)
+    with open('julie.txt') as julie:
+        data = julie.readline()
+        julie_data = data.strip().split(',')
+        print(julie_data)
+    with open('mikey.txt') as mikey:
+        data = mikey.readline()
+        mikey_data = data.strip().split(',')
+        print(mikey_data)
     with open('sarah.txt') as sarah:
-        print(sarah.readline())
+        data = sarah.readline()
+        sarah_data = data.strip().split(',')
+        print(sarah_data)
 
-except FileNotFoundError as fnfe:
-    print("The file has not been found")
+except FileNotFoundError:
+    print("The file hasn't been found")
+except IOError as ioe:
+    print("An error of type " + str(ioe) + " has appeared")
