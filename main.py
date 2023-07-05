b6eee82fa0b79bab4067ae19101755f5fcbc6c66
IñigoBerasa <124163362+Iberasa010@users.noreply.github.com>
2023-07-05 11:58:54 +0200

    Initial commit

--- main.py
+++ main.py
@@ -7,5 +7,12 @@ mickey_list = []
 try:
     with open('julie.txt') as julie:
         print(julie.readline())
+    with open('james.txt') as james:
+        print(james.readline())
+    with open('mickey.txt') as mickey:
+        print(mickey.readline())
+    with open('sarah.txt') as sarah:
+        print(sarah.readline())
+
 except FileNotFoundError as fnfe:
     print("The file has not been found")
