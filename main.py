b52c9f3e997734e7e06ddf10e25bea5ea7701cb3
IñigoBerasa <124163362+Iberasa010@users.noreply.github.com>
2023-07-05 11:56:24 +0200

    Initial commit

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-import pickle
 
-print("hello")
+julie_list = []
+james_list = []
+sarah_list = []
+mickey_list = []
+
+try:
+    with open('julie.txt') as julie:
+        print(julie.readline())
+except FileNotFoundError as fnfe:
+    print("The file has not been found")
