51bdbf7000b2b4b36542b5b4ef0e5f33c5233c5f
IñigoBerasa <124163362+Iberasa010@users.noreply.github.com>
2023-07-07 10:04:41 +0200

    improvement

--- main.py
+++ main.py
@@ -1,36 +1,16 @@
-from sanitizer import sanitizer
 from opener import open_files
 
 try:
-    james_data = open_files('james.txt')
-    mikey_data = open_files('mikey.txt')
-    julie_data = open_files('julie.txt')
-    sarah_data = open_files('sarah.txt')
+    james_data = open_files('james2.txt')
+    mikey_data = open_files('mikey2.txt')
+    julie_data = open_files('julie2.txt')
+    sarah_data = open_files('sarah2.txt')
 
-    sorted_s = [sanitizer(elem) for elem in sarah_data]
-    sorted_ja = [sanitizer(elem) for elem in james_data]
-    sorted_ju = [sanitizer(elem) for elem in julie_data]
-    sorted_m = [sanitizer(elem) for elem in mikey_data]
-
-    james = sorted(set(sorted_s))
-    mikey = sorted(set(sorted_m))
-    julie = sorted(set(sorted_ju))
-    sarah = sorted(set(sorted_ja))
-
-    print("Best 3 James times: ")
-    print(james[0:3])
-    print("Best 3 Mikey times: ")
-    print(mikey[0:3])
-    print("Best 3 Julie times: ")
-    print(julie[0:3])
-    print("Best 3 Sarah times: ")
-    print(sarah[0:3])
-
-    sarah_file = open_files('sarah2.txt')
-    sarah = {'Name': sarah_file.pop(0), 'Birthday': sarah_file.pop(0), 'Times': sarah_file}
-    print(sarah['Name'] + "'s fastest times are: " + str(sorted(set([sanitizer(t) for t in sarah["Times"]]))[0:3]))
-
-    # print(sarah_name + "'s fastest times are: " + str(sorted(set([sanitizer(t) for t in sarah]))[0:3]))
+    print("The athlete's data are: ")
+    print(james_data)
+    print(sarah_data)
+    print(julie_data)
+    print(mikey_data)
 
 except FileNotFoundError:
     print("The file hasn't been found")
--- opener.py
+++ opener.py
@@ -1,13 +1,17 @@
+from sanitizer import sanitizer
+
 
 def open_files(athlete_file):
     # This function opens a file and makes it appropriate to work with in the latter
     # parts of the code.
-
     try:
         with open(athlete_file) as generic_file:
             data = generic_file.readline()
             athlete = data.strip().split(',')
-            return athlete
+            dic_athlete = {'Name': athlete.pop(0), 'Birthday': athlete.pop(0),
+                           "Times": str(sorted(set([sanitizer(t) for t in athlete]))[0:3])}
+            return dic_athlete
+
     except FileNotFoundError:
         print("Such file does not exist")
     except IOError as ioe:
